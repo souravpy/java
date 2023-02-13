8547cf0f62966ea1c6136a38c960bebe6daf1c0c
Sourav Biswal <souravzsn@gmail.com>
2023-02-13 15:31:59 +0530

    long beak

---
+++ hellocli.java
@@ -0,0 +1,10 @@
+public class hellocli {
+        public static void main(String[] args) {
+                System.out.println("Number of command line arguments: " + args.length);
+                for (int i = 0; i < args.length; i++) {
+                        System.out.println("Argument "+i+":"+args[i]);
+                }
+                System.out.println("HEllo cliii");
+       } 
+}
+
---
+++ operators.java
@@ -0,0 +1,19 @@
+import javax.print.event.PrintEvent;
+
+public class operators {
+        public static void main(String[] args) {
+                int result = 1+2;
+                System.out.println(result);
+                
+                int result = 1+2;
+                System.out.println(result);
+
+                int result = 1*2;
+                System.out.println(result);
+                
+                int result = 1/2;
+                System.out.println(result);
+                }
+                
+        }        
+}
