d242e1903d87f8577efb8a6ef46ecb9dec5493b9
Sourav Biswal <souravzsn@gmail.com>
2023-02-14 20:22:08 +0530

    learning java

---
+++ array.java
@@ -0,0 +1,42 @@
+public class array {
+public static void main(String args[]){
+        int[] anArray;
+        
+        anArray = new int[11];
+
+        anArray[0] = 1;
+        anArray[2] = 2;
+        anArray[3] = 3;
+        anArray[4] = 4;
+        anArray[5] = 5;
+        anArray[6] = 6;
+        anArray[7] = 7;
+        anArray[8] = 8;
+        anArray[9] = 9;
+        anArray[10] = 10;
+        anArray[11] = 11;
+
+      System.out.println(anArray[0] = 1);
+      System.out.println(anArray[2] = 2);
+      System.out.println(anArray[3] = 3);
+      System.out.println(anArray[4] = 4);
+      System.out.println(anArray[5] = 5);
+      System.out.println(anArray[6] = 6);
+      System.out.println(anArray[7] = 7);
+      System.out.println(anArray[8] = 8);
+      System.out.println(anArray[9] = 9);
+      System.out.println(anArray[10] = 10);
+
+        anArray[1] = 1;
+        anArray[2] = 2;
+        anArray[3] = 3;
+        anArray[4] = 4;
+        anArray[5] = 5;
+        anArray[6] = 6;
+        anArray[7] = 7;
+        anArray[8] = 8;
+        anArray[9] = 9;
+        anArray[10] = 10;
+        anArray[11] = 11;
+}        
+}
--- operators.java
+++ operators.java
@@ -5,15 +5,18 @@ public class operators {
                 int result = 1+2;
                 System.out.println(result);
                 
-                int result = 1+2;
-                System.out.println(result);
+                String result1 = "1-2";
+                System.out.println(result1);
 
-                int result = 1*2;
-                System.out.println(result);
+                int result2 = 1*2;
+                System.out.println(result2);
                 
-                int result = 1/2;
-                System.out.println(result);
+                int result3 = 1/2;
+                System.out.println(result3);
+
+                int result4 = 1%2;
+                System.out.println(result4);
                 }
                 
         }        
-}
+
