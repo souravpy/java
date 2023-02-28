e6b073d1304a9965399800fb7b2b37ac5a24157a
Sourav Biswal <souravzsn@gmail.com>
2023-02-28 20:19:14 +0530

    java classroom

---
+++ sumofdigit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sumofdigit {
+        public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter a number"); 
+                String input = scanner.next();
+                
+
+        }
+        
+}
---
+++ twoDarray.java
@@ -0,0 +1,9 @@
+public class twoDarray {
+        public static void main (String [] args){
+                System.out.println("enter a string");
+
+        }
+        
+}
+
+//wap to add two matrices
