99ae28449036f128caa35ef6acd5f277be07d997
Sourav Biswal <souravzsn@gmail.com>
2023-04-11 11:54:42 +0530

    class roomj stuff

---
+++ exception.java
@@ -0,0 +1,10 @@
+public class exception {
+        public static void main(String args[]){
+                int a=10;
+                int b=0;
+                int c= a/b;
+        }
+        
+}
+// default handler, handled by jvm, 
+//as exception handler isnt explicitly defined
