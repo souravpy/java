58c3b5f6561ae76d31874352dfa71ff883dc3c47
Sourav Biswal <souravzsn@gmail.com>
2023-04-11 18:39:42 +0530

    class room stuff

---
+++ exception_handler.java
@@ -0,0 +1,18 @@
+import javax.print.event.PrintEvent;
+
+public class exception_handler {
+        class testE
+        {
+                public static void main(String[] args) {
+                        int a=10; b=0; c=0;
+                        try {
+                                c=a/b
+                        } catch (ArithematicException 0) {
+                                // TODO: handle exception
+                        
+                        }
+
+                }
+        }
+        
+}
